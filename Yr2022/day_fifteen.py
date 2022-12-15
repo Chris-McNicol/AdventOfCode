6d8b726524eb7b4b73938b30db7a6571ef9a1fa5
Chris McNicol <christopher.mcnicol101@mod.gov.uk>
2022-12-15 19:15:26 +0000

    improvements to day 15

--- Yr2022/day_fifteen.py
+++ Yr2022/day_fifteen.py
@@ -6,40 +6,53 @@ import itertools
 from tqdm import tqdm
 
 
+def sign(x):
+    return -1 if x < 0 else 1 if x>0 else 0
+
 class Sensor:
-    def __init__(self, own_pos: Tuple[int, int], nrst_b: Tuple[int, int]):
-        self.own_pos = own_pos
+    def __init__(self, pos: Tuple[int, int], nrst_b: Tuple[int, int]):
+        self.pos = pos
         self.nrst_b = nrst_b
         self.taxi = self.taxicab_distance
 
 
     @property
+    def x(self):
+        return self.pos[0]
+
+    @property
+    def y(self):
+        return self.pos[1]
+
+    @property
     def taxicab_distance(self):
-        delta_x, delta_y = self.nrst_b[0]-self.own_pos[0], self.nrst_b[1]-self.own_pos[1]
+        delta_x, delta_y = self.nrst_b[0]-self.pos[0], self.nrst_b[1]-self.pos[1]
         return abs(delta_x) + abs(delta_y)
 
+    def range_from(self, test_pos):
+        delta_x, delta_y = test_pos[0]-self.pos[0], test_pos[1]-self.pos[1]
+        return abs(delta_x) + abs(delta_y)
 
     def in_range(self, test_pos):
-        delta_x, delta_y = test_pos[0]-self.own_pos[0], test_pos[1]-self.own_pos[1]
-        return abs(delta_x) + abs(delta_y) <= self.taxi
+         return self.range_from(test_pos)<= self.taxi
 
 
     def annulus(self):
         result = set()
         for dx in range(0, self.taxi+2):
             dy = self.taxi + 1 - dx
-            result.add((self.own_pos[0]+dx, self.own_pos[1]+dy))
+            result.add((self.pos[0]+dx, self.pos[1]+dy))
             ### Don't need to generate these as can just check NE and SW sectors
-            #result.add((self.own_pos[0]-dx, self.own_pos[1]+dy))
-            #result.add((self.own_pos[0]+dx, self.own_pos[1]-dy))
-            result.add((self.own_pos[0]-dx, self.own_pos[1]-dy))
+            #result.add((self.pos[0]-dx, self.pos[1]+dy))
+            #result.add((self.pos[0]+dx, self.pos[1]-dy))
+            result.add((self.pos[0]-dx, self.pos[1]-dy))
         return result
     
 
     def get_conjugate_coord(self, coord, is_min=True, is_y=True):
-        dist = self.own_pos[1] - coord if is_y else self.own_pos[0] - coord
+        dist = self.pos[1] - coord if is_y else self.pos[0] - coord
         conj_dist = abs(dist)-self.taxi if is_min else self.taxi - abs(dist)
-        return self.own_pos[0] + conj_dist if is_y else self.own_pos[1] + conj_dist
+        return self.pos[0] + conj_dist if is_y else self.pos[1] + conj_dist
 
 
 @dataclass
@@ -72,8 +85,8 @@ class SensorList:
           
 
     def check_row(self, row_idx, speed_mode=True):
-        min_x = min([sens.get_conjugate_coord(row_idx) for sens in self.sensors if abs(sens.own_pos[1]-row_idx) <= sens.taxi ])
-        max_x = max([sens.get_conjugate_coord(row_idx, is_min=False) for sens in self.sensors if abs(sens.own_pos[1]-row_idx) <= sens.taxi  ])
+        min_x = min([sens.get_conjugate_coord(row_idx) for sens in self.sensors if abs(sens.pos[1]-row_idx) <= sens.taxi ])
+        max_x = max([sens.get_conjugate_coord(row_idx, is_min=False) for sens in self.sensors if abs(sens.pos[1]-row_idx) <= sens.taxi  ])
         if speed_mode:
             return max_x - min_x
         in_range_counter = 0
@@ -91,6 +104,29 @@ class SensorList:
                 if not self.is_sensor_in_range((x,y)):
                     return x*big + y                    
         return None
+
+    def search_space_better(self, big):
+        ascend, descend = set(), set()
+        for (a, b) in itertools.combinations(self.sensors, 2):            
+            if a.range_from(b.pos) == a.taxi+b.taxi+2:
+                print(a.pos, b.pos)
+                if sign(a.x-b.x) == sign(a.y-b.y):
+                    a_sum, b_sum = a.x + a.y, b.x + b.y
+                    descend.add(a_sum + sign(b_sum-a_sum)*(a.taxi+1))
+                else:
+                    a_dif, b_dif = a.y - a.x, b.y - b.x
+                    ascend.add(a_dif + sign(b_dif-a_dif)*(a.taxi+1))
+        for (up, dwn) in itertools.product(ascend, descend):
+            if (up % 2 == 0) ^ (dwn % 2 == 0):
+                continue
+            x,y = (dwn-up)/2, (dwn+up)/2
+            for sensor in self.sensors:
+                if sensor.range_from((x,y)) > sensor.taxi:
+                    return int(x*big + y)
+        return None
+
+
+
         
 
 def find_beacons(filename: str):
@@ -98,16 +134,20 @@ def find_beacons(filename: str):
     with open(filename, 'r') as file:
         cmd_lines = file.readlines()
 
+
     sensor_list = SensorList([])
     for line in cmd_lines:
         split_line = line.strip().replace('=',',').replace(':',',').split(',')
         sens_x, sens_y = int(split_line[1]), int(split_line[3])
         b_x, b_y = int(split_line[-3]), int(split_line[-1])
         sensor_list.sensors.append( Sensor((sens_x, sens_y), (b_x,b_y)))
-
+    
+    
     row_idx = 2_000_000
     part_one = sensor_list.check_row(row_idx, speed_mode=True)   
-    part_two = sensor_list.search_space(big = 4_000_000)
+    #part_two = sensor_list.search_space(big = 4_000_000)    
+    part_two = sensor_list.search_space_better(big = 4_000_000)
+
     return part_one, part_two
   
 
@@ -115,5 +155,6 @@ if __name__ == "__main__":
     time_start = perf_counter_ns()
     file_loc = r"C:\Users\Chris\Documents\CodeProjects\AdventOfCode\Yr2022\PuzzleData\puzzle_day_fifteen.txt"     
     part_one, part_two = find_beacons(file_loc)
+    
     print(f"(Part One): {part_one}     (Part Two): {part_two}")   
-    print(f"Elapsed time : {(perf_counter_ns() - time_start)/1e9:.0f} seconds")  
+    print(f"Elapsed time : {(perf_counter_ns() - time_start)/1000:.0f} microseconds")  
--- Yr2022/src/day_fifteen.rs
+++ Yr2022/src/day_fifteen.rs
@@ -3,6 +3,8 @@ use std::io::{BufRead, BufReader};
 use std::time::Instant;
 use std::collections::HashSet;
 use itertools::Itertools;
+use rayon::prelude::*;
+use itertools::iproduct;
 
 
 fn main() {   
@@ -11,10 +13,14 @@ fn main() {
     let (part_one, part_two) = search_beacons(file_name);
     let duration = start.elapsed();    
     println!("Part One: {}  Part Two: {}", part_one, part_two);
-    println!("Time taken to execute: {} seconds", duration.as_secs());    
+    println!("Time taken to execute: {} microseconds", duration.as_micros());    
 }
 
 type Pos = (i64,i64);
+
+fn sign(x: i64) -> i64 {
+    return if x!= 0 {x/x.abs()} else {0};
+}
 
 struct Sensor {
     own_pos: Pos,
@@ -30,15 +36,21 @@ impl Sensor {
         self.taxi = delta_x.abs() + delta_y.abs();
     }
 
-    pub fn in_range(&self, test_pos: Pos) -> bool {
+    pub fn range_from(&self, test_pos: Pos) -> i64 {
         let delta_x = test_pos.0 - self.own_pos.0;
         let delta_y = test_pos.1 - self.own_pos.1;
-        return (delta_x.abs() + delta_y.abs()) <= self.taxi;
+        return delta_x.abs() + delta_y.abs()
+    }
+
+    pub fn in_range(&self, test_pos: Pos) -> bool {
+        return self.range_from(test_pos) <= self.taxi;
     }
 
     pub fn annulus(&self) -> HashSet<Pos> {
         let mut result : HashSet<Pos> = HashSet::new();
-        for dx in 0..=self.taxi+1 {
+
+        
+        for dx in 0..=self.taxi+1 {        
             let dy = self.taxi + 1 - dx;
             result.insert( (self.own_pos.0+dx, self.own_pos.1+dy));
             // Don't need to include these
@@ -46,6 +58,7 @@ impl Sensor {
             //result.insert( (self.own_pos.0+dx, self.own_pos.1-dy));
             result.insert( (self.own_pos.0-dx, self.own_pos.1-dy));
         }
+        
         return result;
     }
 
@@ -78,9 +91,14 @@ impl SensorList {
     pub fn annulus_set(&self) -> HashSet<Pos> {
         let mut result : HashSet<Pos> = HashSet::new();
         let annulus_vec: Vec<HashSet<Pos>> = self.sensors.iter().map(|s| s.annulus() ).collect();
+        
         for (annul_one, annul_two) in annulus_vec.iter().tuple_combinations() {
             result.extend(annul_one.intersection(&annul_two));
         }
+        //let annulus_vec: Vec<HashSet<Pos>> = self.sensors.par_iter().map(|s| s.annulus() ).collect();
+        //annulus_vec.into_iter().tuple_combinations().par_bridge().for_each(|&(annul_one, annul_two)|
+        //                result.extend(annul_one.intersection(&annul_two)));
+
         return result;
     }
 
@@ -90,12 +108,9 @@ impl SensorList {
         if speed_mode {
             return (max_x - min_x) as usize;
         }
-        let mut in_range_counter: usize = 0;
-        for x in min_x..=max_x {
-            if self.is_sensor_in_range((x, row_idx)) && !self.is_beacon((x, row_idx)) {
-                in_range_counter += 1;
-            }
-        }
+        let in_range_counter = (min_x..=max_x).into_par_iter().filter(|&x| {
+            self.is_sensor_in_range((x,row_idx)) && !self.is_beacon((x,row_idx))
+        }).count();
         return in_range_counter;
     }
 
@@ -103,15 +118,54 @@ impl SensorList {
         let (min_x, max_x) : Pos = (0, big);
         let (min_y, max_y) : Pos = (0, big);
         let annulus_set : HashSet<Pos> = self.annulus_set();
-        for pos in annulus_set {
+        return annulus_set.par_iter().find_map_any(|&pos| 
             if (min_x..=max_x).contains(&pos.0) && (min_y..=max_y).contains(&pos.1) {
                 if !self.is_sensor_in_range(pos) {
-                    return Some( pos.0*big + pos.1 );
+                    Some( pos.0*big + pos.1 )
+                }
+                else { None }
+            } 
+            else { None }
+            
+        );
+    }
+
+    pub fn search_space_better(&self, big: i64) -> Option<i64> {
+        let mut ascend: HashSet<i64> = HashSet::new();
+        let mut descend: HashSet<i64> = HashSet::new();
+
+        for (a,b) in self.sensors.iter().tuple_combinations() {
+            if a.range_from(b.own_pos) == a.taxi + b.taxi + 2{
+                if sign(a.own_pos.0 - b.own_pos.0) == sign(a.own_pos.1-b.own_pos.1){
+                    let a_sum = a.own_pos.0 + a.own_pos.1;
+                    let b_sum = b.own_pos.0 + b.own_pos.1;
+                    descend.insert(a_sum + sign(b_sum-a_sum)*(a.taxi+1));
+                }
+                else{
+                    let a_dif = a.own_pos.1 - a.own_pos.0;
+                    let b_dif = b.own_pos.1 - b.own_pos.0;
+                    ascend.insert(a_dif + sign(b_dif-a_dif)*(a.taxi+1));
                 }
             }
         }
+
+        for (up, dwn) in iproduct!(&ascend, &descend){
+            if (up % 2 == 0) ^ (dwn % 2 == 0) { continue;}
+            let x = (dwn-up)/2;
+            let y = (dwn+up)/2;
+            for sensor in &self.sensors {
+                if sensor.range_from((x,y)) > sensor.taxi {
+                    return Some(x*big + y);
+                }
+            }
+
+        }
+
         return None;
-    }
+    }        
+        
+
+    
 }
 
 
@@ -134,7 +188,8 @@ fn search_beacons(my_file_name: &str) -> (usize, i64) {
     let row_idx: i64 = 2000000;
     let part_one = sensor_list.check_row(row_idx, true);
     let mut part_two : i64 = 0 ;
-    let part_two_result = sensor_list.search_space(4000000);
+    //let part_two_result = sensor_list.search_space(4000000);
+    let part_two_result = sensor_list.search_space_better(4000000);
     match part_two_result {
         Some(val) => part_two = val,
         None => println!("Couldn't find a solution!"),
